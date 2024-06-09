e7f713b80b58e52d387b03df787532426b881567
ab0mination <44244370+kkim370@users.noreply.github.com>
2024-06-09 13:52:55 -0400

    Delete reddit_scrape directory

--- reddit_scrape/anti_dup.py
+++
@@ -1,4 +0,0 @@
-with open('reddit_links.txt') as result:
-        uniqlines = set(result.readlines())
-        with open('reddit_links_no_dupes.txt', 'w') as rmdup:
-            rmdup.writelines(set(uniqlines))
